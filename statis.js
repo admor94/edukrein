c632b54530c405ca72132acde9e93b3a02229438
admor94 <124000109+admor94@users.noreply.github.com>
2025-09-19 07:20:33 +0700

    Update statis.js

--- statis.js
+++ statis.js
@@ -1,1 +1,7 @@
-
+document.addEventListener("DOMContentLoaded", function() {
+    var staticWrapper = document.getElementById('statis-wrapper-content');
+    if (staticWrapper) {
+      document.body.appendChild(staticWrapper);
+      document.getElementById('statis-wrapper').remove(); // Hapus div pembungkus awal
+    }
+  });
